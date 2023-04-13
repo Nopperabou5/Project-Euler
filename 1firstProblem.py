d10f59f259ecade51398c601235f00fdd3b0ee4c
Nopperabou5 <maherfauzi9@proton.me>
2023-04-13 10:07:29 +0700

    applying clean code technique

--- 1firstProblem.py
+++ 1firstProblem.py
@@ -1,16 +1,26 @@
-number = 0 
-list_number = []
-total = 0 
-inialization_number = 1000
+START_NUMBER = 1 
+MULTIPLES_LIST = []
+LIMIT = 1000
 
-def multiples_3or5 (number):
-    while number < inialization_number :
-        number+=1
-        if number % 3 == 0  or number % 5 == 0:  
-            list_number.append (number)
-    total = sum(list_number) 
-    print (total)
+def generate_multiples(limit, start_number):
+    """
+    Generates a list of natural numbers that are multiples of 3 or 5 up to a given limit.
+    """
+    number = start_number
+    while number < limit:
+        if number % 3 == 0 or number % 5 == 0:
+            MULTIPLES_LIST.append(number)
+        number += 1
+
+def calculate_sum(numbers_list):
+    """
+    Calculates the sum of a list of numbers.
+    """
+    total = sum(numbers_list)
+    return total
 
 if __name__ == "__main__":
-    multiples_3or5(number)
-
+    generate_multiples(LIMIT, START_NUMBER)
+    print(MULTIPLES_LIST)
+    total_sum = calculate_sum(MULTIPLES_LIST)
+    print(total_sum)
